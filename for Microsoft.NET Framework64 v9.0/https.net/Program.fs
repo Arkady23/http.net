8501ebb47065a32ba245e990d5cee1348b91d09c
Arkady23 <kornienko@outlook.com>
2024-07-04 11:53:04 +1000

    Create Program.fs

---
+++ for Microsoft.NET Framework64 v9.0/https.net/Program.fs
@@ -0,0 +1,22 @@
+/////////////////////////////////////////////////////////
+//    https-сервер на F#.     Автор: A.Б.Корниенко     //
+/////////////////////////////////////////////////////////
+
+namespace Global
+
+open System
+open System.IO
+open System.Web
+open System.Net
+open System.Net.Sockets
+open System.Diagnostics
+open System.Collections.Generic
+open System.Threading
+open System.Threading.Tasks
+open System.Reflection
+open System.Text
+
+[<AutoOpen>]
+module Public =
+
+module Main =
