422fa6a703b8e536cfd05ee0c6ddbbe09c1b5a31
Arkady23 <kornienko@outlook.com>
2024-07-04 11:27:46 +1000

    Create https.net.cs

---
+++ for Microsoft.NET Framework64 v4.0/https/https.net.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Web;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net.Security;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class https{
+}
+
+class Session{
+}
+
+class main{
+}
+
